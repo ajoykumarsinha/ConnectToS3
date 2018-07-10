4282f848580ff70e93af5dd2da9257ed95529299
ajoysinha <ajoy.sinha@live.com>
2018-07-10 09:35:19 +0530

    Add s3 upload

---
+++ src/main/java/aws/example/s3/PutObject.java
@@ -0,0 +1,67 @@
+/*
+   Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+package aws.example.s3;
+import java.io.File;
+import java.nio.file.Paths;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+/**
+ * Upload a file to an Amazon S3 bucket.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
+ */
+public class PutObject
+{
+    public static void main(String[] args)
+    {
+        final String USAGE = "\n" +
+            "To run this example, supply the name of an S3 bucket and a file to\n" +
+            "upload to it.\n" +
+            "\n" +
+            "Ex: PutObject <bucketname> <filename>\n";
+
+        if (args.length < 2) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String bucket_name = args[0];
+        String file_path = args[1];
+        String key_name = Paths.get(file_path).getFileName().toString();
+
+        System.out.format("Uploading %s to S3 bucket %s...\n", file_path, bucket_name);
+        final AmazonS3 s3 = AmazonS3Client.builder()
+            	.withRegion(Regions.AP_SOUTH_1)
+    	        .withCredentials(new ProfileCredentialsProvider("ajoykumarsinha@gmail.com"))
+    	        .build();
+        try {
+        	PutObjectRequest request = new PutObjectRequest(bucket_name, key_name, new File(file_path));
+        	//request.setCannedAcl(CannedAccessControlList.PublicRead);
+            s3.putObject(request);
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        System.out.println("Done!");
+    }
+}
+
